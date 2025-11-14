26c08821151a9b713d4935a60eecb9d5e8d5701f
Stéphane Gamot <stephanegamot@outlook.com>
2025-11-14 23:28:06 +0100

    carrousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import HeroHomePage from "./../components/heroes/HeroHomePage"
 import CardsHome from "@/components/cards/CardsHome";
+import Carrousel from "@/components/carrousel/Carrousel";
 
 export default function Home() {
   return (
 <main>
     <HeroHomePage />
     <CardsHome />
+    <Carrousel />
 </main>
   );
 }
