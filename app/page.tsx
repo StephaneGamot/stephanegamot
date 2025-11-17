5547f723a7239fcc07e3ad52a434dc5708e4d2cd
Stéphane Gamot <stephanegamot@outlook.com>
2025-11-17 23:46:55 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -31,9 +31,8 @@ export const metadata = {
 export default function Home() {
   return (
 <main>
- 
-    <CardsHome />
-    <Carrousel />
+    <HeroHomePage />
+  
     <WorkProcess />
     <TestimonialsSection />
     <AboutSection />
