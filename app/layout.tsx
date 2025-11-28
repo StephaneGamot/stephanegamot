97633bdeeab516ad6d639cd7e9b56cf47f619a5c
Stéphane Gamot <stephanegamot@outlook.com>
2025-11-28 12:25:49 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export const metadata: Metadata = {
         locale: "fr_BE",
         images: [
             {
-                url: "@/public/Images/og/office-workstation.webp",
+              url: "https://www.stephanegamot.com/Images/og/office-workstation.webp",
                 width: 1200,
                 height: 630,
                 alt: "Site web professionnel créé par Stéphane Gamot",
