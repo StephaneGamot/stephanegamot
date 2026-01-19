10081530d4b975246b58fe7b71d9884f673772b2
Stéphane Gamot <stephanegamot@outlook.com>
2026-01-19 20:21:53 +0100

    test

--- app/head.tsx
+++ app/head.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
-
 export default function Head() {
   return (
     <>
+      <meta name="ahrefs-head-test" content="OK" />
       <script
         src="https://analytics.ahrefs.com/analytics.js"
         data-key="GDaY2OVpE6IfdQicYtz/qQ"
